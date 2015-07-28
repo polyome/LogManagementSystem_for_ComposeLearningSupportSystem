4fe4035b984293448b3a78d9160fbdc2ea437ff7
polyome <a1402030@sendai-nct.jp>
2015-07-28 16:57:24 +0900

    test

--- LMSfT/lmtm.js
+++ LMSfT/lmtm.js
@@ -1,20 +1,39 @@
 tm.define("lm",{
 //最初に読み込む　今のところ実質テスト場
+
 init : function(){
 
-console.log(tm.app.CanvasApp.width);
-console.log(tm.display.CanvasApp.height);
+this.hold = new Array();
 
+console.log(this.hold);
 
 },
 
-getmouseXYfromWindowSize : function(){
-	window.onmousemove = handleMouseMove;
-    function handleMouseMove(event) {
-        event = event || window.event;
-        console.log(event.clientX + ", " + event.clientY);
-    }
+//getmouseXYfromWindowSize : function(){
+//	window.onmousemove = handleMouseMove;
+//    function handleMouseMove(event) {
+//        event = event || window.event;
+//        console.log(event.clientX + ", " + event.clientY);
+//    }
+//},
+
+getmouseXY : function(){
+	var p = app.pointing;
+
+	var x = p.x;
+    var y = p.y;
+
+    this.hold.push(x+","+y);
+
 },
 
+play: function(){
+
+    console.log(this.hold)
+},
+
+
+update: function(){
+},
 
 })
